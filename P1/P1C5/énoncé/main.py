3d2a459f5f060679f58b832304e3c831dbfc46e7
BugsBuny04 <gueyendiouga2020@gmail.com>
2024-05-16 00:37:48 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,12 @@
-# Écrivez votre code ici !
+nom="Bugs"
+age=10
+taille=1.7
+est_etudiant= True
+print("nom")
+print("age")
+print("taille")
+print("est_etudiant")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
